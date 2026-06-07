7c1b5cad2a0fa0c2578af1d9b82374a83b288abf
Samuel De Francesco <defrancescodev@gmail.com>
2026-06-07 02:08:35 -0300

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to http-demo-request
+
+TODO: write [great documentation](https://jacobian.org/writing/what-to-write/)
---
+++ test/http_demo_request/core_test.clj
@@ -0,0 +1,7 @@
+(ns http-demo-request.core-test
+  (:require [clojure.test :refer :all]
+            [http-demo-request.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
